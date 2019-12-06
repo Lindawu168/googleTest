f8e16c6cadf0128ade7cdecdf3563874d0f2f68f
Lindawu168 <lindawu16898@gmail.com>
2019-12-06 15:57:21 -0500

    This is Initial commit

---
+++ first_test.js
@@ -0,0 +1,12 @@
+var webdriver = require('selenium-webdriver');
+var browser = new webdriver.Builder().withCapabilities(webdriver.withCapabilities.firefox()).build();
+
+browser.get('http://www.google.com');
+
+var promise= browser.getTitle();
+
+promise.then(function(title) {
+  console.log(title);
+})
+
+browser.quit();
